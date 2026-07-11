006e4c57d76284ed3ef10c29042a6d7d06f7df05
Orit Arny <orit.arny.de.saban@gmail.com>
2026-07-11 21:40:23 +0300

    added ccl korea

---
+++ js/ccl-korea-images.js
@@ -0,0 +1,14 @@
+var cclKoreaProjectImages = [
+    'images/ccl-korea/CIMG2578.JPG',
+    'images/ccl-korea/CIMG2579.JPG',
+    'images/ccl-korea/CIMG2580.JPG',
+    'images/ccl-korea/CIMG2581.JPG',
+    'images/ccl-korea/CIMG2582.JPG',
+    'images/ccl-korea/CIMG2583.JPG',
+    'images/ccl-korea/CIMG2584.JPG',
+    'images/ccl-korea/CIMG2585.JPG',
+    'images/ccl-korea/CIMG2586.JPG',
+    'images/ccl-korea/CIMG2587.JPG',
+    'images/ccl-korea/CIMG2588.JPG',
+    'images/ccl-korea/CIMG2589.JPG',
+];
